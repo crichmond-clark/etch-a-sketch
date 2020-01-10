bf7aaed89074e9b0df6acce194c5a36a000183b4
crichmond-clark <43894793+crichmond-clark@users.noreply.github.com>
2020-01-10 21:05:41 +0000

    added reste + new grid size

--- app.js
+++ app.js
@@ -1,49 +1,59 @@
 const gridContainer = document.querySelector('.grid-container');
 const resetButton = document.querySelector('.reset-button');
 const gridContainerWidth = 960;
+let gridNumber = 16;
+let gridSize = gridNumber * gridNumber;
+let elementHeight = gridContainerWidth / gridNumber;
 
-let gridNumber = 16*16;
-let elementHeight = gridNumber / gridContainerWidth;
-
-
+createGrid(gridSize);
+let gridElem = document.querySelectorAll('.grid-element');
+addBlackClass();
 
 
 function createGridPiece() {
-
   let gridElement = document.createElement('div');
   gridElement.className = "grid-element";
   gridContainer.append(gridElement);
-
-
 };
 
 function createGrid(numOfElements) {
+  gridContainer.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+  gridContainer.style.gridAutoColumns = `${gridContainerWidth/gridNumber}px`;
+  gridContainer.style.gridAutoRows =  `${gridContainerWidth/gridNumber}px`;               
   for (let i = 0; i < numOfElements; i++) {
     createGridPiece()
 
   }
+
+}
+function addBlackClass() {
+  gridElem.forEach((element) => {
+    element.addEventListener('mouseover', () => {
+      element.classList.add('black-bg')
+    });
+  });
 }
 
-
-createGrid(gridNumber);
-
-const gridElem = document.querySelectorAll('.grid-element');
-
-gridElem.forEach((element) => {
-  element.addEventListener('mouseover', () => {
-    element.classList.add('black-bg')
+function removeElements() {
+  gridElem.forEach((element) => {
+    element.remove();
   });
-});
+}
+
 
 resetButton.addEventListener('click', () => {
 
   let userAnswer = parseInt(prompt('How big a grid would you like?'));
-  let gridNumber = userAnswer * userAnswer;
+  gridNumber = userAnswer;
+  gridSize = gridNumber * gridNumber;
+  elementHeight = gridContainerWidth / gridNumber;
+  removeElements();
 
-  gridElem.forEach((element) => {
-    element.remove();
-  });
+  createGrid(gridSize);
+  gridElem = document.querySelectorAll('.grid-element');
+  addBlackClass();
 });
+
 
 
 
